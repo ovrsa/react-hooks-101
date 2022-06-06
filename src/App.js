84358a1bd4340bf1e3cd26d4eb79afe6d4d629df
ovrsa <92124533+ovrsa@users.noreply.github.com>
2022-06-07 08:17:14 +0900

    hello-react-hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>This is template for React App</div>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const Reset = () => setCount(0);
+  const double = () => setCount(count * 2);
+
+  return (
+    <>
+      <div>Count: {count}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={Reset}>Reset</button>
+      <button onClick={double}>×2</button>
+    </>
+  );
 };
 
 export default App;
