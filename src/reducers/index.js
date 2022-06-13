0da517eb82ee54ab28adb6b80009421e9a0fcfae
ovrsa <92124533+ovrsa@users.noreply.github.com>
2022-06-13 17:11:35 +0900

    implement creation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,9 +4,20 @@
 //   body: "2020年に東京でオリンピックを開催します。つきましては。。。",
 // };
 
+// before;
+// state = [];
+
+// after
+// state =
+// {
+//   id:1,
+//   title: "2020東京オリンピックのお知らせ"
+//   body: "2020東京オリンピックを開催いたします！つきましては。。"
+// }
+
 // state = [];
 // state = [
-//   { id: 1, title: "タイトル1", body: "ボディー1" },
+//   { id: 1, title: "タ イトル1", body: "ボディー1" },
 //   { id: 2, title: "タイトル2", body: "ボディー2" },
 //   { id: 3, title: "タイトル3", body: "ボディー3" },
 // ];
@@ -18,10 +29,12 @@ const events = (state = [], action) => {
       const id = length === 0 ? 1 : state[length - 1].id + 1;
       return [...state, { id, ...event }];
     case "DELETE_EVENT":
-      return state;
-    case "CREATE_ALL_EVENTS":
+      return state.filter((event) => event.id !== action.id);
+    case "DELETE_ALL_EVENTS":
       return [];
     default:
       return state;
   }
 };
+
+export default events;
