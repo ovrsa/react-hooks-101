372bc18f69b24c28b1785d716464138eb01340be
ovrsa <92124533+ovrsa@users.noreply.github.com>
2022-06-14 21:03:04 +0900

    separate-components

---
+++ src/components/EventForm.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+const EventForm = ({ state, dispatch }) => {
+  // const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const addEvent = (e) => {
+    e.preventDefault();
+
+    dispatch({
+      type: "CREATE_EVENT",
+      title,
+      body,
+    });
+
+    setTitle("");
+    setBody("");
+  };
+
+  const unCreatable = title === "" || body === "";
+  const deleteAllEvents = (e) => {
+    e.preventDefault();
+    const result = window.confirm("全てのイベントを削除しても本当に良いですか");
+    if (result) dispatch({ type: "DELETE_ALL_EVENTS" });
+  };
+
+  return (
+    <>
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">タイトル</label>
+          <input
+            className="form-control"
+            id="formEventTitle"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="formEventBody">ボディー</label>
+          <textarea
+            className="form-control"
+            id="formEventBody"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
+        </div>
+
+        <button
+          className="btn btn-primary"
+          onClick={addEvent}
+          disabled={unCreatable}
+        >
+          イベント作成
+        </button>
+        <button
+          className="btn btn-danger"
+          onClick={deleteAllEvents}
+          disabled={state.length === 0}
+        >
+          全てのイベントの削除
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default EventForm;
