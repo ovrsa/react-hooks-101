d36f7fb5fe3882d74b9271d86ae2fd48e2e5060f
ovrsa <92124533+ovrsa@users.noreply.github.com>
2022-06-14 21:50:13 +0900

    types-of-action-like-constants

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import { CREATE_EVENT, DELETE_ALL_EVENTS } from "../actions";
+
 const EventForm = ({ state, dispatch }) => {
   // const [state, dispatch] = useReducer(reducer, []);
   const [title, setTitle] = useState("");
@@ -9,7 +11,7 @@ const EventForm = ({ state, dispatch }) => {
     e.preventDefault();
 
     dispatch({
-      type: "CREATE_EVENT",
+      type: CREATE_EVENT,
       title,
       body,
     });
@@ -22,7 +24,7 @@ const EventForm = ({ state, dispatch }) => {
   const deleteAllEvents = (e) => {
     e.preventDefault();
     const result = window.confirm("全てのイベントを削除しても本当に良いですか");
-    if (result) dispatch({ type: "DELETE_ALL_EVENTS" });
+    if (result) dispatch({ type: DELETE_ALL_EVENTS });
   };
 
   return (
